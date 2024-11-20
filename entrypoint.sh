2babc2c78bc702bc27197ed26b416b554b77a815
CMartinez <charli.rey.misterio@gmail.com>
2024-11-20 16:44:18 -0600

    files for deploy

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Ejecutar migraciones y capturar errores
+if ! php artisan migrate --force; then
+    echo "Error: Las migraciones no se ejecutaron correctamente."
+    exit 1
+fi
+
+# Crear enlace simbólico para almacenamiento
+if ! php artisan storage:link; then
+    echo "Error: No se pudo crear el enlace simbólico para el almacenamiento."
+    exit 1
+fi
+
+# Iniciar PHP-FPM
+php-fpm
