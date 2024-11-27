7d1c9a16d7aff5e1f98564f287543ce82c233a0f
CMartinez <devero@github.com>
2024-11-26 20:11:47 -0600

    fixed

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['https://bibliotech-production-8356.up.railway.app/*'],
 
     'allowed_headers' => ['*'],
 
